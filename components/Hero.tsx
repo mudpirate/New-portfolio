4c01d2db63417ad033203084a23bb5a8ea5be7d4
mud pirate <mudpirate3@gmail.com>
2025-09-18 12:13:30 +0530

    fix(Hero): update visibility of background element for responsive design

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
       </div>
       <div
         style={{ width: "100%", height: "100%" }}
-        className="absolute inset-0"
+        className="absolute inset-0 hidden lg:block"
       >
         <LiquidEther
           colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
